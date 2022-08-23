1dba32ac5f8022c4afb7773127420ebc6e4f909e
lch <lch@atguigu.com>
2022-08-23 19:59:46 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author lch
+ * @create 2022-08-23-19:57
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
